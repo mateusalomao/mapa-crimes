55b200f46f8b4ea588ed90c0daedb43060c596f3
Mateus Salomão <91640706+mateusalomao@users.noreply.github.com>
2022-10-19 21:49:51 -0300

    first commit

---
+++ js/dados.js
@@ -0,0 +1,674 @@
+var df = [
+    {
+      LUGAR: "ADUNB/ALUMNI/CASA DO PROF (ESTACIONAMENTO)",
+      LAT: "-15.7667933",
+      LON: "-47.87204504",
+      QUANTIDADE: "4"
+    },
+    {
+      LUGAR: "ADUNB/CASA DO PROFESSOR",
+      LAT: "-15.76712887",
+      LON: "-47.87247956",
+      QUANTIDADE: "3"
+    },
+    {
+      LUGAR: "AGÊNCIA (4290) SANTANDER",
+      LAT: "-15.76681395",
+      LON: "-47.87032843",
+      QUANTIDADE: "7"
+    },
+    {
+      LUGAR: "AGÊNCIA DO BANCO DO BRASIL",
+      LAT: "-15.76514643",
+      LON: "-47.87025332",
+      QUANTIDADE: "110"
+    },
+    {
+      LUGAR: "ALMOXARIFADO CENTRAL (ESTACIONAMENTO)",
+      LAT: "-15.75689638",
+      LON: "-47.86906779",
+      QUANTIDADE: "2"
+    },
+    {
+      LUGAR: "AMARELINHO NORTE",
+      LAT: "-15.75806834",
+      LON: "-47.87105799",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "ANTIGO CLUBE DO SERVIDOR",
+      LAT: "-15.75700996",
+      LON: "-47.86732435",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "ASSOCIAÇÃO DOS PROFESSORES APOSENTADOS DA UNB",
+      LAT: "-15.76640094",
+      LON: "-47.87251711",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "ASSOCIAÇÃO DOS SERVIDORES DA FUNDAÇÃO DA UNIVERSIDADE DE BRASÍLIA",
+      LAT: "-15.75908025",
+      LON: "-47.87341297",
+      QUANTIDADE: "3"
+    },
+    {
+      LUGAR: "BEIJÓDROMO",
+      LAT: "-15.76401581",
+      LON: "-47.8658545",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "BIBLIOTECA CENTRAL",
+      LAT: "-15.76103177",
+      LON: "-47.86789298",
+      QUANTIDADE: "28"
+    },
+    {
+      LUGAR: "BIBLIOTECA CENTRAL (ESTACIONAMENTO DOS FUNCIONÁRIOS)",
+      LAT: "-15.76038127",
+      LON: "-47.86815047",
+      QUANTIDADE: "3"
+    },
+    {
+      LUGAR: "BIBLIOTECA CENTRAL (ESTACIONAMENTO)",
+      LAT: "-15.76188878",
+      LON: "-47.86644459",
+      QUANTIDADE: "27"
+    },
+    {
+      LUGAR: "BIBLIOTECA CENTRAL (PARADA)",
+      LAT: "-15.76057487",
+      LON: "-47.86664575",
+      QUANTIDADE: "3"
+    },
+    {
+      LUGAR: "BIBLIOTECA CENTRAL (VIA)",
+      LAT: "-15.76131056",
+      LON: "-47.86652505",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "BLOCO DE SALA DE AULA SUL - BSA SUL",
+      LAT: "-15.76710305",
+      LON: "-47.86668062",
+      QUANTIDADE: "4"
+    },
+    {
+      LUGAR: "BLOCO DE SALAS DE AULA EUDORO DE SOUSA",
+      LAT: "-15.75695834",
+      LON: "-47.87033379",
+      QUANTIDADE: "2"
+    },
+    {
+      LUGAR: "BLOCOS DE SALA DE AULA NORTE - BSA NORTE",
+      LAT: "-15.75712871",
+      LON: "-47.87100434",
+      QUANTIDADE: "2"
+    },
+    {
+      LUGAR: "BLOCOS DE SALA DE AULA NORTE - BSA NORTE (ESTACIONAMENTO)",
+      LAT: "-15.75726036",
+      LON: "-47.87054569",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "CASA DO ESTUDANTE (L4)",
+      LAT: "-15.76374219",
+      LON: "-47.86259562",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "CASA DO ESTUDANTE UNIVERSITÁRIO",
+      LAT: "-15.76561623",
+      LON: "-47.85766304",
+      QUANTIDADE: "4"
+    },
+    {
+      LUGAR: "CEBRASPE",
+      LAT: "-15.77202809",
+      LON: "-47.86534488",
+      QUANTIDADE: "2"
+    },
+    {
+      LUGAR: "CENTRO COMUNITÁRIO ATHOS BULCÃO",
+      LAT: "-15.75805802",
+      LON: "-47.86833286",
+      QUANTIDADE: "284"
+    },
+    {
+      LUGAR: "CENTRO COMUNITÁRIO ATHOS BULCÃO (ESTACIONAMENTO)",
+      LAT: "-15.7580322",
+      LON: "-47.86754966",
+      QUANTIDADE: "35"
+    },
+    {
+      LUGAR: "CENTRO DE APOIO AO DESENVOLVIMENTO TECNOLÓGICO",
+      LAT: "-15.77455768",
+      LON: "-47.86686301",
+      QUANTIDADE: "3"
+    },
+    {
+      LUGAR: "CENTRO DE ATENDIMENTO E ESTUDOS PSICOLÓGICOS",
+      LAT: "-15.77148603",
+      LON: "-47.87002802",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "CENTRO DE CONVIVÊNCIA",
+      LAT: "-15.76045354",
+      LON: "-47.87370801",
+      QUANTIDADE: "2"
+    },
+    {
+      LUGAR: "CENTRO DE DESENVOLVIMENTO SUSTENTÁVEL (ESTACIONAMENTO)",
+      LAT: "-15.76843499",
+      LON: "-47.86989391",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "CENTRO DE EXCELÊNCIA EM TURISMO",
+      LAT: "-15.76964303",
+      LON: "-47.87026405",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "CENTRO OLÍMPICO",
+      LAT: "-15.76314332",
+      LON: "-47.85843551",
+      QUANTIDADE: "26"
+    },
+    {
+      LUGAR: "CENTRO OLÍMPICO (ESTACIONAMENTO)",
+      LAT: "-15.76472825",
+      LON: "-47.85889149",
+      QUANTIDADE: "20"
+    },
+    {
+      LUGAR: "CIÊNCIA DA COMPUTAÇÃO E ESTATÍSTICA - CIC/EST (ESTACIONAMENTO)",
+      LAT: "-15.7591422",
+      LON: "-47.86921799",
+      QUANTIDADE: "2"
+    },
+    {
+      LUGAR: "COLINA",
+      LAT: "-15.75633879",
+      LON: "-47.87526369",
+      QUANTIDADE: "15"
+    },
+    {
+      LUGAR: "CONCHA ACÚSTICA",
+      LAT: "-15.76566785",
+      LON: "-47.87135839",
+      QUANTIDADE: "3"
+    },
+    {
+      LUGAR: "DEPARTAMENTO DE MÚSICA",
+      LAT: "-15.76451659",
+      LON: "-47.87136376",
+      QUANTIDADE: "3"
+    },
+    {
+      LUGAR: "DEPARTAMENTO DE MÚSICA (ESTACIONAMENTO)",
+      LAT: "-15.76496573",
+      LON: "-47.87101507",
+      QUANTIDADE: "4"
+    },
+    {
+      LUGAR: "EM FRENTE AO MINAS TÊNIS CLUBE",
+      LAT: "-15.75340627",
+      LON: "-47.87096143",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "ENTRE A BIBLIOTECA E A REITORIA",
+      LAT: "-15.76199204",
+      LON: "-47.86747456",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "ENTRE A REITORIA E O ICC SUL",
+      LAT: "-15.76358214",
+      LON: "-47.86785007",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "ENTRE O AMARELINHO NORTE E O BAES",
+      LAT: "-15.75762282",
+      LON: "-47.87117827",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "ESTACIONAMENTO NA UNB PRÓXIMO AO MINAS BRASILIA TENIS CLUBE",
+      LAT: "-15.75446467",
+      LON: "-47.87103653",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "ESTAÇÃO BIOLÓGICA",
+      LAT: "-15.73634209",
+      LON: "-47.88352489",
+      QUANTIDADE: "2"
+    },
+    {
+      LUGAR: "ESTAÇÃO EXPERIMENTAL DE BIOLOGIA",
+      LAT: "-15.74456199",
+      LON: "-47.87699103",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "FACULDADE DE ARQUITETURA E URBANISMO (PRACINHA)",
+      LAT: "-15.76188878",
+      LON: "-47.86959887",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "FACULDADE DE CIÊNCIA DA INFORMAÇÃO",
+      LAT: "-15.76068587",
+      LON: "-47.86744237",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "FACULDADE DE CIÊNCIAS DA SAÚDE/MEDICINA",
+      LAT: "-15.76852792",
+      LON: "-47.86671281",
+      QUANTIDADE: "15"
+    },
+    {
+      LUGAR: "FACULDADE DE CIÊNCIAS DA SAÚDE/MEDICINA (ESTACIONAMENTO)",
+      LAT: "-15.76963786",
+      LON: "-47.86685765",
+      QUANTIDADE: "17"
+    },
+    {
+      LUGAR: "FACULDADE DE CIÊNCIAS DA SAÚDE/MEDICINA (PARADA)",
+      LAT: "-15.76968433",
+      LON: "-47.86714733",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "FACULDADE DE DIREITO",
+      LAT: "-15.75965848",
+      LON: "-47.8721416",
+      QUANTIDADE: "10"
+    },
+    {
+      LUGAR: "FACULDADE DE DIREITO (ESTACIONAMENTO)",
+      LAT: "-15.75965848",
+      LON: "-47.87274241",
+      QUANTIDADE: "23"
+    },
+    {
+      LUGAR: "FACULDADE DE ECONOMIA, ADMINISTRAÇÃO, CONTABILIDADE E GESTÃO PÚBLICA",
+      LAT: "-15.75856397",
+      LON: "-47.87184656",
+      QUANTIDADE: "3"
+    },
+    {
+      LUGAR: "FACULDADE DE ECONOMIA, ADMINISTRAÇÃO, CONTABILIDADE E GESTÃO PÚBLICA (ESTACIONAMENTO)",
+      LAT: "-15.7584349",
+      LON: "-47.8726995",
+      QUANTIDADE: "3"
+    },
+    {
+      LUGAR: "FACULDADE DE EDUCAÇÃO",
+      LAT: "-15.76804264",
+      LON: "-47.87185192",
+      QUANTIDADE: "4"
+    },
+    {
+      LUGAR: "FACULDADE DE EDUCAÇÃO (ESTACIONAMENTO)",
+      LAT: "-15.76851759",
+      LON: "-47.87195921",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "FACULDADE DE EDUCAÇÃO FÍSICA",
+      LAT: "-15.76327755",
+      LON: "-47.8604579",
+      QUANTIDADE: "3"
+    },
+    {
+      LUGAR: "FACULDADE DE EDUCAÇÃO FÍSICA (ESTACIONAMENTO)",
+      LAT: "-15.76381446",
+      LON: "-47.85992146",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "FACULDADE DE EDUCAÇÃO FÍSICA (L4)",
+      LAT: "-15.76317429",
+      LON: "-47.86258221",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "FACULDADE DE TECNOLOGIA",
+      LAT: "-15.76362345",
+      LON: "-47.87255466",
+      QUANTIDADE: "9"
+    },
+    {
+      LUGAR: "FACULDADE DE TECNOLOGIA (ESTACIONAMENTO ICC)",
+      LAT: "-15.76246701",
+      LON: "-47.8726244",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "FACULDADE DE TECNOLOGIA (ESTACIONAMENTO)",
+      LAT: "-15.76407776",
+      LON: "-47.87326813",
+      QUANTIDADE: "21"
+    },
+    {
+      LUGAR: "FAVELINHA DA UNB, VIA L3, ALTURA DA 612 NORTE",
+      LAT: "-15.74837236",
+      LON: "-47.87930846",
+      QUANTIDADE: "2"
+    },
+    {
+      LUGAR: "FUNDAÇÃO DE EMPREENDIMENTOS CIENTÍFICOS E TECNOLÓGICOS - FINATEC",
+      LAT: "-15.7746093",
+      LON: "-47.8710258",
+      QUANTIDADE: "2"
+    },
+    {
+      LUGAR: "FUNDAÇÃO OSWALDO CRUZ",
+      LAT: "-15.77108852",
+      LON: "-47.87115455",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "FUNDAÇÃO OSWALDO CRUZ (L3)",
+      LAT: "-15.7714499",
+      LON: "-47.87212014",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "FUNDAÇÃO UNIVERSIDADE DE BRASÍLIA - FUBRA",
+      LAT: "-15.77600314",
+      LON: "-47.87085414",
+      QUANTIDADE: "2"
+    },
+    {
+      LUGAR: "GARAGEM DA UNB",
+      LAT: "-15.75593093",
+      LON: "-47.87038207",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "HOSPITAL UNIVERSITÁRIO",
+      LAT: "-15.77176997",
+      LON: "-47.87369728",
+      QUANTIDADE: "10"
+    },
+    {
+      LUGAR: "HOSPITAL UNIVERSITÁRIO (ESTACIONAMENTO)",
+      LAT: "-15.77073231",
+      LON: "-47.87274778",
+      QUANTIDADE: "2"
+    },
+    {
+      LUGAR: "HOSPITAL VETERINÁRIO",
+      LAT: "-15.74923975",
+      LON: "-47.87703395",
+      QUANTIDADE: "3"
+    },
+    {
+      LUGAR: "HOSPITAL VETERINÁRIO (L3)",
+      LAT: "-15.75030333",
+      LON: "-47.8780961",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "ICC",
+      LAT: "-15.76355117",
+      LON: "-47.8694272",
+      QUANTIDADE: "11"
+    },
+    {
+      LUGAR: "ICC NORTE",
+      LAT: "-15.76155837",
+      LON: "-47.87029624",
+      QUANTIDADE: "31"
+    },
+    {
+      LUGAR: "ICC NORTE (ESTACIONAMENTO)",
+      LAT: "-15.7617339",
+      LON: "-47.87177682",
+      QUANTIDADE: "23"
+    },
+    {
+      LUGAR: "ICC NORTE (PARADA)",
+      LAT: "-15.76021606",
+      LON: "-47.87062883",
+      QUANTIDADE: "2"
+    },
+    {
+      LUGAR: "ICC SUL",
+      LAT: "-15.76506899",
+      LON: "-47.86791444",
+      QUANTIDADE: "23"
+    },
+    {
+      LUGAR: "ICC SUL (ESTACIONAMENTO)",
+      LAT: "-15.76617379",
+      LON: "-47.86844015",
+      QUANTIDADE: "16"
+    },
+    {
+      LUGAR: "ICC SUL (PARADA)",
+      LAT: "-15.76575045",
+      LON: "-47.86664844",
+      QUANTIDADE: "2"
+    },
+    {
+      LUGAR: "INSTITUTO DE ARTES",
+      LAT: "-15.76566269",
+      LON: "-47.87042499",
+      QUANTIDADE: "10"
+    },
+    {
+      LUGAR: "INSTITUTO DE ARTES (ESTACIONAMENTO)",
+      LAT: "-15.7657711",
+      LON: "-47.87009239",
+      QUANTIDADE: "2"
+    },
+    {
+      LUGAR: "INSTITUTO DE CIÊNCIA POLÍTICA",
+      LAT: "-15.75822323",
+      LON: "-47.86956668",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "INSTITUTO DE CIÊNCIA POLÍTICA (ESTACIONAMENTO)",
+      LAT: "-15.75868788",
+      LON: "-47.86950767",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "INSTITUTO DE CIÊNCIAS BIOLÓGICAS",
+      LAT: "-15.76648354",
+      LON: "-47.86506057",
+      QUANTIDADE: "3"
+    },
+    {
+      LUGAR: "INSTITUTO DE CIÊNCIAS BIOLÓGICAS (ESTACIONAMENTO)",
+      LAT: "-15.76723212",
+      LON: "-47.86411107",
+      QUANTIDADE: "4"
+    },
+    {
+      LUGAR: "INSTITUTO DE CIÊNCIAS BIOLÓGICAS (PARADA)",
+      LAT: "-15.76533228",
+      LON: "-47.8660208",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "INSTITUTO DE CIÊNCIAS SOCIAIS",
+      LAT: "-15.75748494",
+      LON: "-47.87283361",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "INSTITUTO DE CIÊNCIAS SOCIAIS (ESTACIONAMENTO)",
+      LAT: "-15.7570461",
+      LON: "-47.87309647",
+      QUANTIDADE: "4"
+    },
+    {
+      LUGAR: "INSTITUTO DE QUÍMICA",
+      LAT: "-15.76847629",
+      LON: "-47.86477089",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "INSTITUTO DE QUÍMICA (ESTACIONAMENTO)",
+      LAT: "-15.76894092",
+      LON: "-47.86524832",
+      QUANTIDADE: "3"
+    },
+    {
+      LUGAR: "INVASÃO PRÓXIMA À COLINA DA UNB",
+      LAT: "-15.7527764",
+      LON: "-47.87748992",
+      QUANTIDADE: "2"
+    },
+    {
+      LUGAR: "LABORATÓRIO DE ENGENHARIA CIVIL (ESTACIONAMENTO)",
+      LAT: "-15.76528582",
+      LON: "-47.87273705",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "LABORATÓRIO DE ENGENHARIA ELÉTRICA",
+      LAT: "-15.76588468",
+      LON: "-47.87215769",
+      QUANTIDADE: "2"
+    },
+    {
+      LUGAR: "MALOCA DA UNB",
+      LAT: "-15.75995276",
+      LON: "-47.87328422",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "NÚCLEO DE MEDICINA TROPICAL",
+      LAT: "-15.76978758",
+      LON: "-47.86781788",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "NÚCLEO DE MEDICINA TROPICAL (ESTACIONAMENTO)",
+      LAT: "-15.77019541",
+      LON: "-47.86805928",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "PARADA PRÓXIMO AO IBAMA /UNB",
+      LAT: "-15.76810459",
+      LON: "-47.86304355",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "PAVILHÃO ANÍSIO TEIXEIRA",
+      LAT: "-15.75905444",
+      LON: "-47.8708005",
+      QUANTIDADE: "5"
+    },
+    {
+      LUGAR: "PAVILHÃO ANÍSIO TEIXEIRA (ESTACIONAMENTO)",
+      LAT: "-15.75932806",
+      LON: "-47.87110358",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "PAVILHÃO JOÃO CALMON",
+      LAT: "-15.75845039",
+      LON: "-47.8702426",
+      QUANTIDADE: "5"
+    },
+    {
+      LUGAR: "PAVILHÃO JOÃO CALMON (ESTACIONAMENTO)",
+      LAT: "-15.75916285",
+      LON: "-47.87003338",
+      QUANTIDADE: "4"
+    },
+    {
+      LUGAR: "PAVILHÃO JOÃO CALMON/IREL (ESTACIONAMENTO)",
+      LAT: "-15.75838844",
+      LON: "-47.86986709",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "PAVILHÃO MULTIUSO I",
+      LAT: "-15.76678814",
+      LON: "-47.86955059",
+      QUANTIDADE: "13"
+    },
+    {
+      LUGAR: "POSTO DE GASOLINA DA UNB",
+      LAT: "-15.76080461",
+      LON: "-47.87424982",
+      QUANTIDADE: "6"
+    },
+    {
+      LUGAR: "POSTO POLICIAL",
+      LAT: "-15.75893569",
+      LON: "-47.87503302",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "PRAÇA CHICO MENDES",
+      LAT: "-15.76669521",
+      LON: "-47.86993682",
+      QUANTIDADE: "6"
+    },
+    {
+      LUGAR: "PREDIO COPP (SEGURANÇA)",
+      LAT: "-15.76758317",
+      LON: "-47.87012994",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "PREFEITURA DO CAMPUS",
+      LAT: "-15.7599889",
+      LON: "-47.87445366",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "REITORIA",
+      LAT: "-15.7629523",
+      LON: "-47.86695957",
+      QUANTIDADE: "7"
+    },
+    {
+      LUGAR: "REITORIA (ESTACIONAMENTO)",
+      LAT: "-15.76348922",
+      LON: "-47.86636949",
+      QUANTIDADE: "12"
+    },
+    {
+      LUGAR: "REITORIA (L4)",
+      LAT: "-15.76087689",
+      LON: "-47.86425054",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "RESTAURANTE UNIVERSITÁRIO",
+      LAT: "-15.76419134",
+      LON: "-47.87045717",
+      QUANTIDADE: "16"
+    },
+    {
+      LUGAR: "RESTAURANTE UNIVERSITÁRIO (ESTACIONAMENTO)",
+      LAT: "-15.76406743",
+      LON: "-47.8710258",
+      QUANTIDADE: "1"
+    },
+    {
+      LUGAR: "TEATRO ARENA HONESTINO GUIMARÃES",
+      LAT: "-15.76171325",
+      LON: "-47.8691268",
+      QUANTIDADE: "2"
+    }
+  ]
